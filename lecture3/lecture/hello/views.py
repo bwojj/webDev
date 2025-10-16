6bf9664e3d54124583bd7ce1118a1b6c2a93778f
Blake <wojobeemer@Blakes-MacBook-Pro-2.local>
2025-10-16 10:03:02 -0400

    hello app

--- lecture3/lecture/hello/views.py
+++ lecture3/lecture/hello/views.py
@@ -9,4 +9,6 @@ def blake(request):
     return HttpResponse("Hello, Blake!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize(),
+    })
